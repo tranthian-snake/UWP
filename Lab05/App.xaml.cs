ce915fc974d1ceac2bf2bc318c1e23470e41d84a
an <54616933+tranthian-snake@users.noreply.github.com>
2020-09-04 22:44:31 +0700

    Add files via upload

--- Lab05/App.xaml.cs
+++ Lab05/App.xaml.cs
@@ -66,7 +66,7 @@ namespace Lab05
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(ScrollViewerExample), e.Arguments);
+                    rootFrame.Navigate(typeof(HamburgerHeavenChallenge), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();

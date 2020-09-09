c1f1b140757308f29cbde8a1389e2055503067d3
an <54616933+tranthian-snake@users.noreply.github.com>
2020-09-09 19:01:40 +0700

    UWP

---
+++ Lab07/GridViewListView/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridViewListView.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string CoverImage { get; set; }
+    }
+}

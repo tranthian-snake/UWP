5d6fb22ba25d4f0ee26e185a93ad78082c4ece10
an <54616933+tranthian-snake@users.noreply.github.com>
2020-09-18 16:02:44 +0700

    UWP

---
+++ Practical/Practice/Model/Images.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Model
+{
+    public class Images
+    {
+        public string NameImg { get; set; }
+        public string ImgPath { get; set; }
+    }
+}
---
+++ Practical/Practice/Model/Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Model
+{
+    public class Products
+    {
+        public string Product { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
